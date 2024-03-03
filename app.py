5c063bf2c7b902c6460baad511775a85f1089a68
inderkiranbhamra <140848814+inderkiranbhamra@users.noreply.github.com>
2024-03-03 22:59:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, send_file
+from flask import Flask, request, jsonify, redirect, url_for, send_file
 import yagmail
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
 import secrets
 import os
 
@@ -19,7 +18,7 @@ app_password = 'krhu cexv lyue dmnz'  # replace with your generated app password
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 credentials = ServiceAccountCredentials.from_json_keyfile_name('bgmi-registration-e1d0ccd3b338.json', scope)
 gc = gspread.authorize(credentials)
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1EcrnzJ5Po5jSnEZTMxOpNNFZX01lkXdR6S1yG-4ownU/edit#gid=0'
+spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1kn5aWOgR4JP59dZS2WJcsIPkO2Wjr3m-KT-vFyf_hmg/edit?pli=1#gid=0'
 sh = gc.open_by_url(spreadsheet_url)
 worksheet = sh.sheet1  # Assuming you are working with the first sheet
 
@@ -33,35 +32,108 @@ def generate_token():
 
 
 # Function to generate an authentication link with token
-# Function to generate an authentication link with token
-def generate_auth_link(token, username, uid, team_name):
+def generate_auth_link(token, team_name, college_name, leader_name, leader_ign, leader_discord_tag, leader_rank,
+                       leader_contact, leader_email, p2_name, p2_ign, p2_discord_tag, p2_rank, p2_contact, p2_email,
+                       p3_name, p3_ign, p3_discord_tag, p3_rank, p3_contact, p3_email, p4_name, p4_ign, p4_discord_tag,
+                       p4_rank, p4_contact, p4_email, p5_name, p5_ign, p5_discord_tag, p5_rank, p5_contact, p5_email):
     # Replace spaces with underscores in each parameter
-    username = username.replace(' ', '_')
     team_name = team_name.replace(' ', '_')
-    
-    # Construct the authentication link with modified parameters
-    auth_link = f'https://emailauthenticationflask.vercel.app/verify/{token}?username={username}&uid={uid}&team_name={team_name}'
-    
-    return auth_link
+    college_name = college_name.replace(' ', '_')
+    leader_name = leader_name.replace(' ', '_')
+    leader_ign = leader_ign.replace(' ', '_')
+    leader_discord_tag = leader_discord_tag.replace(' ', '_')
+    leader_rank = leader_rank.replace(' ', '_')
+    leader_contact = leader_contact.replace(' ', '_')  # Added space
+    leader_email = leader_email.replace(' ', '_')  # Added space
+    p2_name = p2_name.replace(' ', '_')
+    p2_ign = p2_ign.replace(' ', '_')
+    p2_discord_tag = p2_discord_tag.replace(' ', '_')
+    p2_rank = p2_rank.replace(' ', '_')  # Added space
+    p2_contact = p2_contact.replace(' ', '_')  # Added space
+    p2_email = p2_email.replace(' ', '_')  # Added space
+    p3_name = p3_name.replace(' ', '_')
+    p3_ign = p3_ign.replace(' ', '_')
+    p3_discord_tag = p3_discord_tag.replace(' ', '_')
+    p3_rank = p3_rank.replace(' ', '_')  # Added space
+    p3_contact = p3_contact.replace(' ', '_')  # Added space
+    p3_email = p3_email.replace(' ', '_')  # Added space
+    p4_name = p4_name.replace(' ', '_')
+    p4_ign = p4_ign.replace(' ', '_')
+    p4_discord_tag = p4_discord_tag.replace(' ', '_')
+    p4_rank = p4_rank.replace(' ', '_')  # Added space
+    p4_contact = p4_contact.replace(' ', '_')  # Added space
+    p4_email = p4_email.replace(' ', '_')  # Added space
+    p5_name = p5_name.replace(' ', '_')
+    p5_ign = p5_ign.replace(' ', '_')
+    p5_discord_tag = p5_discord_tag.replace(' ', '_')
+    p5_rank = p5_rank.replace(' ', '_')  # Added space
+    p5_contact = p5_contact.replace(' ', '_')  # Added space
+    p5_email = p5_email.replace(' ', '_')  # Added space
 
+    # Construct the authentication link with modified parameters
+    auth_link = f'https://emailauthenticationflask.vercel.app/verify/{token}?team_name={team_name}&college_name={college_name}&leader_name={leader_name}&leader_ign={leader_ign}&leader_discord_tag={leader_discord_tag}&leader_rank={leader_rank}&leader_contact={leader_contact}&leader_email={leader_email}&p2_name={p2_name}&p2_ign={p2_ign}&p2_discord_tag={p2_discord_tag}&p2_rank={p2_rank}&p2_contact={p2_contact}&p2_email={p2_email}&p3_name={p3_name}&p3_ign={p3_ign}&p3_discord_tag={p3_discord_tag}&p3_rank={p3_rank}&p3_contact={p3_contact}&p3_email={p3_email}&p4_name={p4_name}&p4_ign={p4_ign}&p4_discord_tag={p4_discord_tag}&p4_rank={p4_rank}&p4_contact={p4_contact}&p4_email={p4_email}&p5_name={p5_name}&p5_ign={p5_ign}&p5_discord_tag={p5_discord_tag}&p5_rank={p5_rank}&p5_contact={p5_contact}&p5_email={p5_email}'
+    return auth_link
 
 # Route to handle form submission and send authentication email
 @app.route('/send_email', methods=['POST'])
 def send_email():
     if request.method == 'POST':
-        username = request.form.get('username')
-        email = request.form['email']
-        uid = request.form.get('uid')
         team_name = request.form.get('team_name')
+        college_name = request.form.get('college_name')
+        leader_name = request.form.get('leader_name')
+        leader_ign = request.form.get('leader_ign')
+        leader_discord_tag = request.form.get('leader_discord_tag')
+        leader_rank = request.form.get('leader_rank')
+        leader_contact = request.form.get('leader_contact')
+        leader_email = request.form.get('leader_email')
+        p2_name = request.form.get('p2_name')
+        p2_ign = request.form.get('p2_ign')
+        p2_discord_tag = request.form.get('p2_discord_tag')
+        p2_rank = request.form.get('p2_rank')
+        p2_contact = request.form.get('p2_contact')
+        p2_email = request.form.get('p2_email')
+        p3_name = request.form.get('p3_name')
+        p3_ign = request.form.get('p3_ign')
+        p3_discord_tag = request.form.get('p3_discord_tag')
+        p3_rank = request.form.get('p3_rank')
+        p3_contact = request.form.get('p3_contact')
+        p3_email = request.form.get('p3_email')
+        p4_name = request.form.get('p4_name')
+        p4_ign = request.form.get('p4_ign')
+        p4_discord_tag = request.form.get('p4_discord_tag')
+        p4_rank = request.form.get('p4_rank')
+        p4_contact = request.form.get('p4_contact')
+        p4_email = request.form.get('p4_email')
+        p5_name = request.form.get('p5_name')
+        p5_ign = request.form.get('p5_ign')
+        p5_discord_tag = request.form.get('p5_discord_tag')
+        p5_rank = request.form.get('p5_rank')
+        p5_contact = request.form.get('p5_contact')
+        p5_email = request.form.get('p5_email')
 
         token = generate_token()
+        email = leader_email  # Assuming leader's email is used for verification
         email_tokens[email] = token
 
         # Construct authentication link with all required parameters
-        auth_link = generate_auth_link(token, username, uid, team_name)
-
+        auth_link = generate_auth_link(token, team_name, college_name, leader_name, leader_ign, leader_discord_tag,
+                                       leader_rank, leader_contact, leader_email, p2_name, p2_ign, p2_discord_tag,
+                                       p2_rank, p2_contact, p2_email, p3_name, p3_ign, p3_discord_tag, p3_rank,
+                                       p3_contact, p3_email, p4_name, p4_ign, p4_discord_tag, p4_rank, p4_contact,
+                                       p4_email, p5_name, p5_ign, p5_discord_tag, p5_rank, p5_contact, p5_email)
         subject = 'Authentication Link'
-        body = f'Click on the following link to authenticate: {auth_link}'
+        body = f'''
+                <html>
+                <head>
+                    <title>{subject}</title>
+                </head>
+                <body>
+                    <h2>{subject}</h2>
+                    <p>Click the button below to authenticate:</p>
+                    <a href="{auth_link}" >Authenticate</a>
+                </body>
+                </html>
+                '''
 
         # Create yagmail SMTP client
         yag = yagmail.SMTP(sender_email, app_password)
@@ -81,25 +153,66 @@ def email_sent():
 # Route to handle verification
 @app.route('/verify/<token>', methods=['GET'])
 def verify(token):
-    if token in email_tokens.values():
-        # Authentication successful, store data into Google Sheets
-        email = [key for key, value in email_tokens.items() if value == token][0]
-        del email_tokens[email]  # Remove token from dictionary after verification
+    if token:
+        # Check if the token exists in email_tokens
+        if token in email_tokens.values():
+            # Get the email associated with the token
+            email = [key for key, value in email_tokens.items() if value == token][0]
 
-        # Retrieve data from the query parameters
-        username = request.args.get('username')
-        uid = request.args.get('uid')
-        team_name = request.args.get('team_name')
+            # Retrieve data from the query parameters
+            team_name = request.args.get('team_name')
+            college_name = request.args.get('college_name')
+            leader_name = request.args.get('leader_name')
+            leader_ign = request.args.get('leader_ign')
+            leader_discord_tag = request.args.get('leader_discord_tag')
+            leader_rank = request.args.get('leader_rank')
+            leader_contact = request.args.get('leader_contact')
+            leader_email = request.args.get('leader_email')
+            p2_name = request.args.get('p2_name')
+            p2_ign = request.args.get('p2_ign')
+            p2_discord_tag = request.args.get('p2_discord_tag')
+            p2_rank = request.args.get('p2_rank')
+            p2_contact = request.args.get('p2_contact')
+            p2_email = request.args.get('p2_email')
+            p3_name = request.args.get('p3_name')
+            p3_ign = request.args.get('p3_ign')
+            p3_discord_tag = request.args.get('p3_discord_tag')
+            p3_rank = request.args.get('p3_rank')
+            p3_contact = request.args.get('p3_contact')
+            p3_email = request.args.get('p3_email')
+            p4_name = request.args.get('p4_name')
+            p4_ign = request.args.get('p4_ign')
+            p4_discord_tag = request.args.get('p4_discord_tag')
+            p4_rank = request.args.get('p4_rank')
+            p4_contact = request.args.get('p4_contact')
+            p4_email = request.args.get('p4_email')
+            p5_name = request.args.get('p5_name')
+            p5_ign = request.args.get('p5_ign')
+            p5_discord_tag = request.args.get('p5_discord_tag')
+            p5_rank = request.args.get('p5_rank')
+            p5_contact = request.args.get('p5_contact')
+            p5_email = request.args.get('p5_email')
 
-        # Check if all required parameters are present
-        if username and uid and team_name:
-            # Append new data to Google Sheets
-            new_row = [username, email, uid, team_name]
-            worksheet.append_row(new_row)
-
-            return jsonify({'message': 'Authentication successful. Data stored into Google Sheets.'})
+            # Check if all required parameters are present
+            if team_name and college_name and leader_name and leader_ign and leader_discord_tag and leader_rank \
+                    and leader_contact and leader_email and p2_name and p2_ign and p2_discord_tag and p2_rank \
+                    and p2_contact and p2_email and p3_name and p3_ign and p3_discord_tag and p3_rank and p3_contact \
+                    and p3_email and p4_name and p4_ign and p4_discord_tag and p4_rank and p4_contact and p4_email \
+                    and p5_name and p5_ign and p5_discord_tag and p5_rank and p5_contact and p5_email:
+                # Append new data to Google Sheets
+                new_row = [team_name, college_name, leader_name, leader_ign, leader_discord_tag, leader_rank,
+                           leader_contact, leader_email, p2_name, p2_ign, p2_discord_tag, p2_rank, p2_contact,
+                           p2_email, p3_name, p3_ign, p3_discord_tag, p3_rank, p3_contact, p3_email, p4_name,
+                           p4_ign, p4_discord_tag, p4_rank, p4_contact, p4_email, p5_name, p5_ign, p5_discord_tag,
+                           p5_rank, p5_contact, p5_email]
+                worksheet.append_row(new_row)
+                # Remove token from dictionary after verification
+                del email_tokens[email]
+                return jsonify({'message': 'Authentication successful. Data stored into Google Sheets.'})
+            else:
+                return jsonify({'message': 'Missing parameters in the verification link.'}), 400
         else:
-            return jsonify({'message': 'Missing parameters in the verification link.'}), 400
+            return jsonify({'message': 'Invalid or expired verification link.'}), 400
     else:
-        return jsonify({'message': 'Invalid or expired verification link.'}), 400
+        return jsonify({'message': 'No token provided.'}), 400
 
